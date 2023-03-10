e52ecb6de796b74f2a92372c45aef0708c7d712e
Mohammed Hayder <mr.hamood277@gmail.com>
2023-03-10 23:13:52 +0300

    add to cart functionality working

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { AddBtn, MinusBtn } from '../components/Icons/Controls'
+import { AddBtn, MinusBtn, TrashBtn } from '../components/Icons/Controls'
 import { MAX_QUANTITY } from '../constants'
-import { useCart } from '../contexts/CartContext'
+import { Item, useCart } from '../contexts/CartContext'
 
 const Product = () => {
-  const [count, setCount] = useState(1)
-
-  const { addItem, inCart, setCartMetadata } = useCart()
   const { id } = useParams()
+
+  const { items, addItem, removeItem, inCart, updateItemQuantity } = useCart()
   const alreadyAdded = inCart(id!)
 
   const product = {
@@ -62,18 +61,20 @@ const Product = () => {
           <span className='ml-2 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold'>
             {product.rating.toFixed(1)}
           </span>
-          {[...Array(product.rating).keys()].map((_star: any, idx: number) => (
-            <svg
-              aria-hidden='true'
-              className='h-5 w-5 text-yellow-300'
-              fill='currentColor'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'
-              key={idx}
-            >
-              <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
-            </svg>
-          ))}
+          {[...Array(Math.round(product.rating)).keys()].map(
+            (_star: any, idx: number) => (
+              <svg
+                key={idx}
+                aria-hidden='true'
+                className='h-5 w-5 text-yellow-300'
+                fill='currentColor'
+                viewBox='0 0 20 20'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
+              </svg>
+            )
+          )}
         </div>
 
         <div className='flex flex-col gap-y-2 pb-6'>
@@ -94,18 +95,36 @@ const Product = () => {
           </p>
 
           {alreadyAdded ? (
-            <div className='flex justify-center w-24 bg-gray-100 py-2 px-5 rounded-full'>
-              <AddBtn onClick={() => setCount(prev => (prev < 10 ? prev + 1 : prev))} />
-              <input
-                className='text-center w-8 bg-gray-100'
-                type='number'
-                value={count}
-                min={1}
-                max={MAX_QUANTITY}
-                onChange={e => setCount(parseInt(e.target.value))}
-              />
-              <MinusBtn onClick={() => setCount(prev => (prev > 1 ? prev - 1 : prev))} />
-            </div>
+            <>
+              {items
+                .filter((item: Item) => item.id === id)
+                .map((item: Item) => (
+                  <div key={item.id} className='flex items-center gap-x-2'>
+                    <TrashBtn
+                      className='w-5 h-5 fill-red-600'
+                      onClick={() => removeItem(item.id)}
+                    />
+                    <div className='flex justify-center w-24 bg-gray-100 py-2 px-5 rounded-full'>
+                      <AddBtn
+                        onClick={() => updateItemQuantity(item.id, item.quantity! + 1)}
+                      />
+                      <input
+                        className='text-center w-8 bg-gray-100'
+                        type='number'
+                        value={item.quantity}
+                        min={1}
+                        max={MAX_QUANTITY}
+                        onChange={e =>
+                          updateItemQuantity(item.id, parseInt(e.target.value))
+                        }
+                      />
+                      <MinusBtn
+                        onClick={() => updateItemQuantity(item.id, item.quantity! - 1)}
+                      />
+                    </div>
+                  </div>
+                ))}
+            </>
           ) : (
             <button
               type='button'
