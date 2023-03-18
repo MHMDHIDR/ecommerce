09f6486efea2004f1eda508dbcd1f3185d950b36
Mohammed Hayder <mr.hamood277@gmail.com>
2023-03-18 22:43:39 +0300

    changes

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,13 @@
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-  host: 'localhost',
+  host: 'sql844.main-hosting.eu',
   user: 'u421310157_ecommerce',
   password: 'u421310157_eCommerce',
   database: 'u421310157_ecommerce'
+})
+
+db.connect(function (err) {
+  if (err) throw err
+  console.log('Database is connected successfully !')
 })
--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express'
 import 'dotenv/config'
 import fileUpload from 'express-fileupload'
 import cors from 'cors'
+import { db } from './db'
 
 const app = express()
 
@@ -27,5 +28,16 @@ app.get('/', (_req: Request, res: Response) =>
     `<body style='overflow:hidden;word-spacing:2rem;height:100vh;display:grid;place-items:center;font-size:3em;font-weight:bold;color:white;background-color:#222'>WELCOME TO eCommerce API</body>`
   )
 )
+
+app.post('/', (req, res) => {
+  const q = 'INSERT INTO books(`title`) VALUES (?)'
+
+  const values = [req.body.title]
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err)
+    return res.json(data)
+  })
+})
 
 app.listen(PORT || 4000, () => console.log(`start listening on port : ${PORT || 4000}`))
