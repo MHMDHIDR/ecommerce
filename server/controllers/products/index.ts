f8e0ced597a28e65fb244ee14532c9419aabe883
Mohammed Hayder <mr.hamood277@gmail.com>
2023-03-22 21:53:39 +0300

    imports fixed

--- server/controllers/products/index.ts
+++ server/controllers/products/index.ts
@@ -1,7 +1,7 @@
-import { getProduct } from './getProduct'
-import { getProducts } from './getProducts'
-import { updateProduct } from './updateProduct'
-import { deleteProduct } from './deleteProduct'
-import { addProduct } from './addProduct'
+import { getProduct } from './getProduct.js'
+import { getProducts } from './getProducts.js'
+import { updateProduct } from './updateProduct.js'
+import { deleteProduct } from './deleteProduct.js'
+import { addProduct } from './addProduct.js'
 
 export { getProduct, getProducts, updateProduct, deleteProduct, addProduct }
