a0a2d2a80368bd7c6b71cce95e04566c79c2a812
Mohammed Hayder <mr.hamood277@gmail.com>
2023-03-22 14:49:38 +0300

    changes

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -6,8 +6,7 @@ const db = mysql.createPool({
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
+  waitForConnections: true
 })
 
 export default db
