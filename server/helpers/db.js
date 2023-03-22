18799d4c68d0e446f27acf97482a81ebccf67b35
Mohammed Hayder <mr.hamood277@gmail.com>
2023-03-22 18:38:12 +0300

    changes

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,20 +1,12 @@
 import mysql from 'mysql2'
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASS,
-  database: DB_NAME
-  //,connectionLimit: 10
-})
-
-db.connect(function (err, _) {
-  if (err) {
-    console.log('ERROR: ' + err.message)
-    throw err
-  }
-  console.log('connected to DB')
+  database: DB_NAME,
+  connectionLimit: 10
 })
 
 export default db
