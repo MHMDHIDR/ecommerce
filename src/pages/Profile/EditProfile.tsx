5d08322623e27b41a5e81dc7ce9be3e41381a1ae
Mohammed Hayder <mr.hamood277@gmail.com>
2023-04-01 16:16:13 +0300

    added Layout component to editProfile page

--- src/pages/Profile/EditProfile.tsx
+++ src/pages/Profile/EditProfile.tsx
@@ -112,129 +112,133 @@ const EditProfile = () => {
   }
 
   return !loadingFetch !== null && fetchedUser !== null ? (
-    <section className='container overflow-x-hidden px-5 rtl mx-auto max-w-6xl'>
-      <div className='hidden'>
-        {updateStatus === 1
-          ? notify({
-              type: 'success',
-              msg: updateMsg,
-              reloadIn: TIME_TO_EXECUTE,
-              reloadTo: goTo('edit')
-            })
-          : updateStatus === 0
-          ? notify({ type: 'error', msg: updateMsg })
-          : null}
-      </div>
-      {isSmallScreen && <BackButton to='/' className='absolute z-50 top-6 left-6' />}
+    <Layout>
+      <section className='container overflow-x-hidden px-5 rtl mx-auto max-w-6xl'>
+        <div className='hidden'>
+          {updateStatus === 1
+            ? notify({
+                type: 'success',
+                msg: updateMsg,
+                reloadIn: TIME_TO_EXECUTE,
+                reloadTo: goTo('edit')
+              })
+            : updateStatus === 0
+            ? notify({ type: 'error', msg: updateMsg })
+            : null}
+        </div>
+        {isSmallScreen && <BackButton to='/' className='absolute z-50 top-6 left-6' />}
 
-      <form
-        className='relative flex flex-col items-center'
-        onSubmit={handleUpdateProfile}
-      >
-        <label htmlFor='uploadImg' className='flex items-center gap-y-2 flex-col'>
-          <FileUpload
-            data={{
-              defaultImg: [
-                {
-                  ImgDisplayName: fetchedUser?.username + ' profile',
-                  ImgDisplayPath: fetchedUser?.avatarUrl
+        <form
+          className='relative flex flex-col items-center'
+          onSubmit={handleUpdateProfile}
+        >
+          <label htmlFor='uploadImg' className='flex items-center gap-y-2 flex-col'>
+            <FileUpload
+              data={{
+                defaultImg: [
+                  {
+                    ImgDisplayName: fetchedUser?.username + ' profile',
+                    ImgDisplayPath: fetchedUser?.avatarUrl
+                  }
+                ],
+                imgName: fetchedUser?.username + ' profile'
+              }}
+            />
+          </label>
+
+          <label htmlFor='username' className='form__group'>
+            <span className='form__label'>الاســـــــــم</span>
+            <input
+              type='text'
+              id='username'
+              className='form__input'
+              onChange={e => setUserFullName(e.target.value)}
+              defaultValue={`${fetchedUser?.firstname} ${fetchedUser?.lastname}`}
+              required
+            />
+          </label>
+          <div className='form__group'>
+            <span className='form__label'>النــــــــــوع</span>
+            <label className='form__group cursor-pointer' htmlFor='male'>
+              <input
+                className='form__input'
+                type='radio'
+                id='male'
+                name='gender'
+                value='male'
+                checked={
+                  userGender ? userGender === 'male' : fetchedUser.gender === 'male'
                 }
-              ],
-              imgName: fetchedUser?.username + ' profile'
-            }}
-          />
-        </label>
-
-        <label htmlFor='username' className='form__group'>
-          <span className='form__label'>الاســـــــــم</span>
-          <input
-            type='text'
-            id='username'
-            className='form__input'
-            onChange={e => setUserFullName(e.target.value)}
-            defaultValue={`${fetchedUser?.firstname} ${fetchedUser?.lastname}`}
-            required
-          />
-        </label>
-        <div className='form__group'>
-          <span className='form__label'>النــــــــــوع</span>
-          <label className='form__group cursor-pointer' htmlFor='male'>
+                onChange={() => setUserGender('male')}
+              />
+              <span>ذكر</span>
+            </label>
+            <label className='form__group cursor-pointer' htmlFor='female'>
+              <input
+                className='form__input'
+                type='radio'
+                id='female'
+                name='gender'
+                value='female'
+                checked={
+                  userGender ? userGender === 'female' : fetchedUser.gender === 'female'
+                }
+                onChange={() => setUserGender('female')}
+              />
+              <span>أنثى</span>
+            </label>
+          </div>
+          <label htmlFor='tel' className='form__group'>
+            <span className='form__label'>الهاتــــــف</span>
             <input
-              className='form__input'
-              type='radio'
-              id='male'
-              name='gender'
-              value='male'
-              checked={userGender ? userGender === 'male' : fetchedUser.gender === 'male'}
-              onChange={() => setUserGender('male')}
+              type='tel'
+              id='tel'
+              className='form__input ltr text-right'
+              onChange={e => setTel(e.target.value)}
+              defaultValue={fetchedUser ? fetchedUser?.phone : phone}
+              required
             />
-            <span>ذكر</span>
           </label>
-          <label className='form__group cursor-pointer' htmlFor='female'>
-            <input
-              className='form__input'
-              type='radio'
-              id='female'
-              name='gender'
-              value='female'
-              checked={
-                userGender ? userGender === 'female' : fetchedUser.gender === 'female'
-              }
-              onChange={() => setUserGender('female')}
-            />
-            <span>أنثى</span>
-          </label>
-        </div>
-        <label htmlFor='tel' className='form__group'>
-          <span className='form__label'>الهاتــــــف</span>
-          <input
-            type='tel'
-            id='tel'
-            className='form__input ltr text-right'
-            onChange={e => setTel(e.target.value)}
-            defaultValue={fetchedUser ? fetchedUser?.phone : phone}
-            required
-          />
-        </label>
-        <div className='p-3 border border-gray-400 rounded-xl w-full my-10 space-y-7'>
-          <BorderLink>
-            <span>اللغة</span>
-          </BorderLink>
-          <BorderLink>
-            <span>الاشعارات</span>
-          </BorderLink>
-          <BorderLink icon={<ThemeToggler />}>
-            <label htmlFor='toggler'>الوضع الداكن</label>
-          </BorderLink>
-        </div>
-        <div className='flex items-center gap-x-20'>
-          <button
-            type='submit'
-            className={`min-w-fit bg-green-600 hover:bg-green-700 text-white py-1.5 px-6 rounded-md${
-              isUpdating || updateStatus === 1
-                ? ' disabled:opacity-30 disabled:cursor-not-allowed'
-                : ''
-            }`}
-          >
-            {isUpdating ? (
-              <>
-                <LoadingSpinner />
-                &nbsp; جارِ التحديث...
-              </>
-            ) : (
-              'تحديث'
-            )}
-          </button>
-          <button
-            type='button'
-            onClick={() => handleLogout('/')}
-            className='min-w-fit bg-blue-600 hover:bg-blue-700 text-white py-1.5 px-6 rounded-md'
-          >
-            تسجيل الخروج
-          </button>
-        </div>
-      </form>
-    </section>
+          <div className='p-3 border border-gray-400 rounded-xl w-full my-10 space-y-7'>
+            <BorderLink>
+              <span>اللغة</span>
+            </BorderLink>
+            <BorderLink>
+              <span>الاشعارات</span>
+            </BorderLink>
+            <BorderLink icon={<ThemeToggler />}>
+              <label htmlFor='toggler'>الوضع الداكن</label>
+            </BorderLink>
+          </div>
+          <div className='flex items-center gap-x-20'>
+            <button
+              type='submit'
+              className={`min-w-fit bg-green-600 hover:bg-green-700 text-white py-1.5 px-6 rounded-md${
+                isUpdating || updateStatus === 1
+                  ? ' disabled:opacity-30 disabled:cursor-not-allowed'
+                  : ''
+              }`}
+            >
+              {isUpdating ? (
+                <>
+                  <LoadingSpinner />
+                  &nbsp; جارِ التحديث...
+                </>
+              ) : (
+                'تحديث'
+              )}
+            </button>
+            <button
+              type='button'
+              onClick={() => handleLogout('/')}
+              className='min-w-fit bg-blue-600 hover:bg-blue-700 text-white py-1.5 px-6 rounded-md'
+            >
+              تسجيل الخروج
+            </button>
+          </div>
+        </form>
+      </section>
+    </Layout>
   ) : (
     <LoadingPage />
   )
