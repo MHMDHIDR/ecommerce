b1775a5f6fb140b691bd5bda99ea4398322f921a
Mohammed Hayder <mr.hamood277@gmail.com>
2023-04-09 01:30:57 +0300

    jsx fixes

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CategoryProducts from '@/components/CategoryProducts'
 import BackButton from '@/components/Icons/BackButton'
@@ -21,48 +21,48 @@ const Categories = () => {
     if (response !== null) setProducts(response)
   }, [response])
 
-  return (
-    <Suspense fallback={<LoadingPage />}>
-      <Layout>
-        <section className='container px-7 mx-auto my-10 flex flex-col rtl mb-24 max-w-6xl'>
-          <div className='flex gap-x-7 items-center justify-between'>
-            <Search />
-            {isSmallScreen && <BackButton to='/' className='w-8 h-8' />}
-          </div>
-          {name ? (
-            <CategoryProducts name={name} products={products} />
-          ) : (
-            <div className='flex flex-wrap justify-center mt-5 gap-3 md:gap-12'>
-              {CATEGORIES.map(
-                (
-                  {
-                    ar_label,
-                    en_label,
-                    itemCount
-                  }: { ar_label: string; en_label: string; itemCount: number },
-                  idx: number
-                ) => (
-                  <Link
-                    key={idx}
-                    to={en_label}
-                    className='block overflow-hidden transition-transform duration-300 bg-cover w-80 h-24 md:h-60 rounded-2xl hover:-translate-y-2'
+  return loading ? (
+    <LoadingPage />
+  ) : (
+    <Layout>
+      <section className='container px-7 mx-auto my-10 flex flex-col rtl mb-24 max-w-6xl'>
+        <div className='flex gap-x-7 items-center justify-between'>
+          <Search />
+          {isSmallScreen && <BackButton to='/' className='w-8 h-8' />}
+        </div>
+        {name ? (
+          <CategoryProducts name={name} products={products} />
+        ) : (
+          <div className='flex flex-wrap justify-center mt-5 gap-3 md:gap-12'>
+            {CATEGORIES.map(
+              (
+                {
+                  ar_label,
+                  en_label,
+                  itemCount
+                }: { ar_label: string; en_label: string; itemCount: number },
+                idx: number
+              ) => (
+                <Link
+                  key={idx}
+                  to={en_label}
+                  className='block overflow-hidden transition-transform duration-300 bg-cover w-80 h-24 md:h-60 rounded-2xl hover:-translate-y-2'
+                >
+                  <div
+                    className={`flex items-center pr-3 justify-left h-full text-sm font-bold bg-gray-800 md:text-base 2xl:text-xl bg-opacity-70 bg-[url("/assets/img/logo.png")] bg-cover bg-[left]`}
                   >
-                    <div
-                      className={`flex items-center pr-3 justify-left h-full text-sm font-bold bg-gray-800 md:text-base 2xl:text-xl bg-opacity-70 bg-[url("/assets/img/logo.png")] bg-cover bg-[left]`}
-                    >
-                      <div className='flex flex-col bg-black bg-opacity-60 px-2 py-0.5 text-white rounded'>
-                        <span>{ar_label}</span>
-                        <span>{itemCount} منتج</span>
-                      </div>
+                    <div className='flex flex-col bg-black bg-opacity-60 px-2 py-0.5 text-white rounded'>
+                      <span>{ar_label}</span>
+                      <span>{itemCount} منتج</span>
                     </div>
-                  </Link>
-                )
-              )}
-            </div>
-          )}
-        </section>
-      </Layout>
-    </Suspense>
+                  </div>
+                </Link>
+              )
+            )}
+          </div>
+        )}
+      </section>
+    </Layout>
   )
 }
 
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import useDocumentTitle from '@/hooks/useDocumentTitle'
 import { LoadingPage } from '@/components/Loading'
@@ -9,57 +9,66 @@ import Arrow from '@/components/Icons/Arrow'
 import CategoryProducts from '@/components/CategoryProducts'
 import { isSmallScreen, PRODUCT } from '@/constants'
 import abstractText from '@/utils/abstractText'
+import { ProductProps } from '@/types'
+import { useAxios } from '@/hooks/useAxios'
 
 const Home = () => {
   useDocumentTitle('المنتجات')
   const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false)
 
-  return (
-    <Suspense fallback={<LoadingPage />}>
-      <Layout>
-        <section className='container px-5 mx-auto h-full max-w-6xl'>
-          <main className='rtl'>
-            <div className='flex justify-between gap-x-5 items-center'>
-              <Search />
-              <Filter
-                className='w-6 h-6 min-w-fit dark:fill-white hover:cursor-pointer'
-                onClick={() => setIsFilterOpen(prev => !prev)}
+  const [products, setProducts] = useState<ProductProps[]>([PRODUCT('1')])
+  const { response, loading } = useAxios({ url: `/products?status=open` })
+
+  useEffect(() => {
+    if (response !== null) setProducts(response)
+  }, [response])
+
+  return loading ? (
+    <LoadingPage />
+  ) : (
+    <Layout>
+      <section className='container px-5 mx-auto h-full max-w-6xl'>
+        <main className='rtl'>
+          <div className='flex justify-between gap-x-5 items-center'>
+            <Search />
+            <Filter
+              className='w-6 h-6 min-w-fit dark:fill-white hover:cursor-pointer'
+              onClick={() => setIsFilterOpen(prev => !prev)}
+            />
+          </div>
+
+          <div>
+            <h2 className='my-3 font-bold'>الأكثر رواجاً</h2>
+            <Link
+              to={`/product/${PRODUCT('1').id}`}
+              className='flex items-center gap-x-3 overflow-hidden rounded-lg bg-white dark:bg-gray-700 px-1.5 shadow-md'
+            >
+              <img
+                className='h-16 w-16 rounded-lg object-cover'
+                src={PRODUCT('1').imgUrl}
+                alt={PRODUCT('1').itemName}
               />
-            </div>
-
-            <div>
-              <h2 className='my-3 font-bold'>الأكثر رواجاً</h2>
-              <Link
-                to={`/product/${PRODUCT('1').id}`}
-                className='flex items-center gap-x-3 overflow-hidden rounded-lg bg-white dark:bg-gray-700 px-1.5 shadow-md'
-              >
-                <img
-                  className='h-16 w-16 rounded-lg object-cover'
-                  src={PRODUCT('1').imgUrl}
-                  alt={PRODUCT('1').itemName}
-                />
-                <div className='py-2'>
-                  <h5 className='text-md font-semibold text-gray-800 dark:text-gray-50'>
-                    {PRODUCT('1').itemName}
-                  </h5>
-                  <p className='text-sm text-gray-600 dark:text-gray-50'>
-                    {abstractText(PRODUCT('1').description, isSmallScreen ? 45 : 200)}
-                  </p>
-                  <span className='text-md font-bold text-gray-800 dark:text-gray-50'>
-                    {PRODUCT('1').currentPrice} ج.س
-                  </span>
-                </div>
-                <span className='flex items-center rounded-md bg-gray-800 dark:bg-gray-200 p-2 mr-auto'>
-                  <Arrow className='fill-white dark:fill-gray-800' />
+              <div className='py-2'>
+                <h5 className='text-md font-semibold text-gray-800 dark:text-gray-50'>
+                  {PRODUCT('1').itemName}
+                </h5>
+                <p className='text-sm text-gray-600 dark:text-gray-50'>
+                  {abstractText(PRODUCT('1').description, isSmallScreen ? 45 : 200)}
+                </p>
+                <span className='text-md font-bold text-gray-800 dark:text-gray-50'>
+                  {PRODUCT('1').currentPrice} ج.س
                 </span>
-              </Link>
-            </div>
+              </div>
+              <span className='flex items-center rounded-md bg-gray-800 dark:bg-gray-200 p-2 mr-auto'>
+                <Arrow className='fill-white dark:fill-gray-800' />
+              </span>
+            </Link>
+          </div>
 
-            <CategoryProducts category={'جميع المنتجات'} />
-          </main>
-        </section>
-      </Layout>
-    </Suspense>
+          <CategoryProducts category={'جميع المنتجات'} products={products} />
+        </main>
+      </section>
+    </Layout>
   )
 }
 
